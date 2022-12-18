9266a8bcc10a9c9db58e88dd85dba39ea38589b2
Mostafa Mahmoud <mostafa201272@yahoo.com>
2022-12-18 16:02:52 +0200

    Update Users Forms

--- Users/forms.py
+++ Users/forms.py
@@ -7,7 +7,7 @@ class UserRegisteration(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["first_name","username", "email", "password1", "password2"]
+        fields = ["first_name", "last_name","username", "email", "password1", "password2"]
     
 
     def __init__(self, *args, **kwargs):
@@ -16,15 +16,23 @@ class UserRegisteration(UserCreationForm):
         # First name Field
         self.fields['first_name'].widget.attrs['placeholder'] = 'First name'
         self.fields['first_name'].widget.attrs['required'] = 'true'
+        
+        # Last name Field
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Last name'
+        self.fields['last_name'].widget.attrs['required'] = 'true'
 
         # Username Field
         self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['username'].widget.attrs['required'] = 'true'
 
         # Email Field
         self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['email'].widget.attrs['required'] = 'true'
 
         # Password Field
         self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password1'].widget.attrs['required'] = 'true'
 
         # confirm password Field
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm password'
+        self.fields['password2'].widget.attrs['required'] = 'true'
