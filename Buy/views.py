faccac914584945a02b66891e1f3362d9662be29
mostafa201272 <mostafa201272@yahoo.com>
2022-12-24 01:57:28 +0200

    Update Buy page

--- Buy/views.py
+++ Buy/views.py
@@ -7,45 +7,31 @@ import geocoder
 def buy_search_page(request):
 
     temp = """
-    
-            <!-- START OF ITEM -->
-            <div class="col-md-6">
-                <!-- ITEM -->
-                <div class="room-list-item">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <figure class="gradient-overlay-hover link-icon">
-                                <a href="#"><img src="/static/frontend/images/rooms/single/single1.jpg" class="img-fluid" alt="Image"></a>
-                            </figure>
-                        </div>
-                        <div class="col-lg-12">
-                            <div class="room-info">
-                                <h3 class="room-title">
-                                    <a href="#">SR 890K</a>
-                                </h3>
-                                
-                                <p>
-                                    <!-- Location Icon -->
-                                    <span>
-                                        <svg xmlns="http://www.w3.org/2000/svg" style="width: 18px;" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
-                                    </span>
 
-                                    <!-- Location -->
-                                    <span>17011 Hidden Treasure Cir, Friendswood, TX 77546</span>
-                                </p>
-                                <div class="room-services">
-                                    <span>Area: 150m<sup>2</sup></span>
-                                    <span>Beds: 5</span>
-                                    <span>Bathrooms: 2</span>
-                                </div>
-                            </div>
-                        </div>
+            <a href="#" target="_blank" class="map-card-container">
+   
+                <div class="map-card-image">
+                    <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/brewster-mcleod-architects-1486154143.jpg" alt="HOUSE">
+                </div>
+                
+                <div class="map-card-details">
+                    <div class="map-card-details-price">
+                    <span>SR 950K</span>
+                    </div>
+                    <div class="map-card-details-location">
+                        <p>
+                            <!-- Location Icon -->
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" style="width: 18px;" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
+                            </span>
+
+                            <!-- Location -->
+                            <span>17011 Hidden Treasure Cir, Friendswood, TX 77546</span>
+                        </p>
                     </div>
                 </div>
-            </div>
-            <!-- END OF ITEM -->
-
-
+                
+            </a>
     """
 
     # Create Location Object
@@ -59,11 +45,7 @@ def buy_search_page(request):
 
     # Create Marker
     # folium.Marker([lat, lng], tooltip="view", popup=temp, icon=folium.Icon(color="orange", icon='home')).add_to(buy_map)
-    folium.Marker([30, 30], tooltip="view", popup=temp, icon=folium.Icon(color="orange")).add_to(buy_map)
-    
-
-    # GEO Jason
-    
+    folium.Marker([30, 30], popup=temp, icon=folium.Icon(color="red", icon="home")).add_to(buy_map)
 
     # Generate HTML MAP
     buy_map.default_css.append(("buhrah_styles","/static/frontend/css/main.css",))
