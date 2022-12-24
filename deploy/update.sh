80493265dacc394193ad12b20564b002cb313077
mostafa201272 <mostafa201272@yahoo.com>
2022-12-24 02:10:39 +0200

    Update CSS Font

--- deploy/update.sh
+++ deploy/update.sh
@@ -6,7 +6,7 @@ PROJECT_BASE_PATH='https://github.com/mostafa201272/buhrah.git'
 
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+$PROJECT_BASE_PATH/env/bin/python managet.py collectstatic --noinpu
 supervisorctl restart buhrah
 
 echo "DONE! :)"
